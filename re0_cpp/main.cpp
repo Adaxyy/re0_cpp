72c46027a2d49ccf9d7f3e7c5ccdcc00bcf20c1e
adaxyy <rex_xyy@outlook.com>
2024-03-11 15:56:34 +0800

    Initial Commit

---
+++ re0_cpp/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  re0_cpp
+//
+//  Created by GB Xue on 2024/3/11.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
